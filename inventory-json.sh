90dba44f8ce667e062530760a02228f67c3fcd0e
Dustin Cote <dustin@confluent.io>
2018-03-06 16:41:06 -0500

    first poc commit

--- inventory-json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat inventory.json
--- terraform_inventory.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo nounset -o pipefail                      # Treat unset variables as an error
-INVENTORY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ati "$@" --root /Users/antony/repos/exmples/multi-region-clusters
