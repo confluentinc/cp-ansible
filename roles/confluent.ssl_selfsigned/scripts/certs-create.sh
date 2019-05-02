38ad2aa3382e7366064e5490ecc2bfc5a63c553e
Justin Manchester <justin@confluent.io>
2019-05-02 13:31:28 +0000

    new file:   all.retry 	modified:   all.yml 	modified:   roles/confluent.ssl_selfsigned/scripts/certs-create.sh 	modified:   roles/confluent.ssl_selfsigned/tasks/main.yml 	new file:   ssl_selfsigned/vars/.security_vars.yml.swp 	modified:   ssl_selfsigned/vars/security_vars.yml

--- roles/confluent.ssl_selfsigned/scripts/certs-create.sh
+++ roles/confluent.ssl_selfsigned/scripts/certs-create.sh
@@ -17,22 +17,22 @@ do
 	# Create host keystore
 	keytool -genkey -noprompt \
 				 -alias $i \
-				 -dname "CN=$i,OU=$3,O=$4,L=$5,S=$6,C=$7" \
+				 -dname "CN=$3,OU=$4,O=$5,L=$6,S=$7,C=$8" \
                                  -ext san=dns:$i \
 				 -keystore $i.keystore.jks \
 				 -keyalg RSA \
-				 -storepass $8 \
-				 -keypass $9
+				 -storepass $9 \
+				 -keypass $10
 
 
         # Import the CA cert into the keystore
-	keytool -noprompt -keystore $i.keystore.jks -alias CARoot -import -file $1  -storepass $8 -keypass $9
+	keytool -noprompt -keystore $i.keystore.jks -alias CARoot -import -file $1  -storepass $9 -keypass $10
 
         # Import the host certificate into the keystore
-	keytool -noprompt -keystore $i.keystore.jks -alias $i -import -file $2 -storepass $8 -keypass $9
+	keytool -noprompt -keystore $i.keystore.jks -alias $i -import -file $2 -storepass $9 -keypass $10
 
 	# Create truststore and import the CA cert
-	keytool -noprompt -keystore $i.truststore.jks -alias CARoot -import -file $1 -storepass $8 -keypass $9
+	keytool -noprompt -keystore $i.truststore.jks -alias CARoot -import -file $1 -storepass $9 -keypass $10
 
 	# Save creds
   	echo "confluent" > ${i}_sslkey_creds
@@ -43,10 +43,10 @@ do
 	#   openssl x509 -noout -modulus -in client.certificate.pem | openssl md5
 	#   openssl rsa -noout -modulus -in client.key | openssl md5 
         #   echo "GET /" | openssl s_client -connect localhost:8082/subjects -cert client.certificate.pem -key client.key -tls1 
-	keytool -export -alias $i -file $i.der -keystore $i.keystore.jks -storepass $8
+	keytool -export -alias $i -file $i.der -keystore $i.keystore.jks -storepass $9
 	openssl x509 -inform der -in $i.der -out $i.certificate.pem
-	keytool -importkeystore -srckeystore $i.keystore.jks -destkeystore $i.keystore.p12 -deststoretype PKCS12 -deststorepass $8 -srcstorepass $8 -noprompt
-	openssl pkcs12 -in $i.keystore.p12 -nodes -nocerts -out $i.key -passin pass:$9
+	keytool -importkeystore -srckeystore $i.keystore.jks -destkeystore $i.keystore.p12 -deststoretype PKCS12 -deststorepass $9 -srcstorepass $9 -noprompt
+	openssl pkcs12 -in $i.keystore.p12 -nodes -nocerts -out $i.key -passin pass:$10
 
 done
 
