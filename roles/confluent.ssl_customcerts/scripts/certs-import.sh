3ccdd5c08f63aaa52aace30c4801df2c3e824d15
Justin Manchester <justin.manchester@gmail.com>
2019-07-25 12:10:05 +0100

    Removed extraneous comments.

--- roles/confluent.ssl_customcerts/scripts/certs-import.sh
+++ roles/confluent.ssl_customcerts/scripts/certs-import.sh
@@ -30,13 +30,9 @@ do
   	echo "confluent" > ${i}_truststore_creds
 
 	# Create pem files and keys used for Schema Registry HTTPS testing
-	#   openssl x509 -noout -modulus -in client.certificate.pem | openssl md5
-	#   openssl rsa -noout -modulus -in client.key | openssl md5 
-        #   echo "GET /" | openssl s_client -connect localhost:8082/subjects -cert client.certificate.pem -key client.key -tls1 
 	keytool -export -alias host -file $i.der -keystore $i.keystore.jks -storepass $4
 	openssl x509 -inform der -in $i.der -out $i.certificate.pem
 	keytool -importkeystore -srckeystore $i.keystore.jks -destkeystore $i.keystore.p12 -deststoretype PKCS12 -deststorepass $4 -srcstorepass $4 -noprompt
-	#openssl pkcs12 -in $i.keystore.p12 -nodes -nocerts -out $i.key -passin pass:$4
 
 done
 
