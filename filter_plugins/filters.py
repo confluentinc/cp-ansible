7ac8c791da065b4b4e7954c327fdaf7e2aa4b941
Domenic Bove <domenicbove@gmail.com>
2019-10-23 17:48:21 -0700

    first step at creating custom filter, this should help in making a list of listeners that can be iterated over with a loop

---
+++ filter_plugins/filters.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+class FilterModule(object):
+    def filters(self):
+        return {
+            'normalize_sasl_protocol': self.normalize_sasl_protocol,
+            'kafka_protocol': self.kafka_protocol
+        }
+
+    def normalize_sasl_protocol(self, protocol):
+        normalized = 'GSSAPI' if protocol.lower() == 'kerberos' \
+            else 'SCRAM-SHA-256' if protocol.upper() == 'SCRAM' \
+            else 'PLAIN' if protocol.upper() == 'PLAIN' \
+            else 'none'
+        return normalized
+
+    def kafka_protocol(self, sasl_protocol_normalized, ssl_enabled):
+        kafka_protocol = 'SASL_SSL' if ssl_enabled and sasl_protocol_normalized in ['GSSAPI', 'PLAIN', 'SCRAM-SHA-256'] \
+            else 'SASL_PLAINTEXT' if not ssl_enabled and sasl_protocol_normalized in ['GSSAPI', 'PLAIN', 'SCRAM-SHA-256'] \
+            else 'SSL' if ssl_enabled and sasl_protocol_normalized == 'none' \
+            else 'PLAINTEXT'
+        return kafka_protocol
