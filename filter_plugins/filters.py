b06bd2c581cb7554f3e4a985b13a1da9069aa520
Domenic Bove <domenicbove@gmail.com>
2019-10-24 11:18:28 -0700

    made one more filter that calls into normalize, to simplify code

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -3,6 +3,7 @@ class FilterModule(object):
     def filters(self):
         return {
             'normalize_sasl_protocol': self.normalize_sasl_protocol,
+            'kafka_protocol_normalized': self.kafka_protocol_normalized,
             'kafka_protocol': self.kafka_protocol
         }
 
@@ -13,9 +14,14 @@ class FilterModule(object):
             else 'none'
         return normalized
 
-    def kafka_protocol(self, sasl_protocol_normalized, ssl_enabled):
+    def kafka_protocol_normalized(self, sasl_protocol_normalized, ssl_enabled):
         kafka_protocol = 'SASL_SSL' if ssl_enabled and sasl_protocol_normalized in ['GSSAPI', 'PLAIN', 'SCRAM-SHA-256'] \
             else 'SASL_PLAINTEXT' if not ssl_enabled and sasl_protocol_normalized in ['GSSAPI', 'PLAIN', 'SCRAM-SHA-256'] \
             else 'SSL' if ssl_enabled and sasl_protocol_normalized == 'none' \
             else 'PLAINTEXT'
+        return kafka_protocol
+
+    def kafka_protocol(self, sasl_protocol, ssl_enabled):
+        sasl_protocol_normalized = self.normalize_sasl_protocol(sasl_protocol)
+        kafka_protocol = self.kafka_protocol_normalized(sasl_protocol_normalized, ssl_enabled)
         return kafka_protocol
