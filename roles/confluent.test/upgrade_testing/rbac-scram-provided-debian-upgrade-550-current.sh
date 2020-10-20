76c40af78ce38bb664a150fa0913a50e6dda519c
Justin Manchester <justin.manchester@gmail.com>
2020-10-20 16:51:57 +0100

    Corrected branches.

--- roles/confluent.test/upgrade_testing/rbac-scram-provided-debian-upgrade-550-current.sh
+++ roles/confluent.test/upgrade_testing/rbac-scram-provided-debian-upgrade-550-current.sh
@@ -5,8 +5,8 @@ set -e
 ## Variables
 
 export SCENARIO_NAME=rbac-scram-provided-debian
-export START_BRANCH=upgrade_testing
-export END_BRANCH=6.0.0-post
+export START_BRANCH=5.5.0-post
+export END_BRANCH=upgrade_testing
 export START_UPGRADE_VERSION=5.5
 export END_UPGRADE_VERSION=6.0
 
