4dfe2faff0d0b6cb6cb7639880954386cbee1eca
Justin Manchester <justin.manchester@gmail.com>
2020-10-21 18:05:00 +0100

    Added upgrade scenarios for testing upgrades from 5.4.0 to 5.5.0.

---
+++ roles/confluent.test/upgrade_testing/plaintext-rhel-upgrade-540-current.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+## Variables
+
+export SCENARIO_NAME=plaintext-rhel
+export START_BRANCH=5.4.0-post
+export START_UPGRADE_VERSION=5.4
+export END_UPGRADE_VERSION=5.0
+
+echo "Call upgrade script"
+sh ./upgrade.sh
