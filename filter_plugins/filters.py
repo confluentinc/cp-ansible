3e025f8d1e49b4b9758828e76439dff754f2f389
Domenic Bove <domenicbove@gmail.com>
2021-01-26 21:22:54 -0800

    should be working w other components now

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -109,7 +109,7 @@ class FilterModule(object):
         return final_dict
 
     # TODO pkcs12_enabled logic should be reworked
-    def listener_properties(self, listeners_dict, default_ssl_enabled, default_pkcs12_enabled, default_ssl_mutual_auth_enabled, default_sasl_protocol,
+    def listener_properties(self, listeners_dict, default_ssl_enabled, keystore_type, default_ssl_mutual_auth_enabled, default_sasl_protocol,
                             kafka_broker_truststore_path, kafka_broker_truststore_storepass, kafka_broker_keystore_path, kafka_broker_keystore_storepass, kafka_broker_keystore_keypass,
                             plain_jaas_config, keytab_path, kerberos_principal, kerberos_primary,
                             scram_user, scram_password, oauth_pem_path ):
@@ -126,7 +126,8 @@ class FilterModule(object):
                 final_dict['listener.name.' + listener_name + '.ssl.keystore.password'] = kafka_broker_keystore_storepass
                 final_dict['listener.name.' + listener_name + '.ssl.key.password'] = kafka_broker_keystore_keypass
 
-            if listeners_dict[listener].get('pkcs12_enabled', default_pkcs12_enabled):
+            # if listeners_dict[listener].get('pkcs12_enabled', default_pkcs12_enabled):
+            if keystore_type == 'BCFKS':
                 final_dict['listener.name.' + listener_name + '.ssl.keymanager.algorithm'] = 'PKIX'
                 final_dict['listener.name.' + listener_name + '.ssl.trustmanager.algorithm'] = 'PKIX'
                 final_dict['listener.name.' + listener_name + '.ssl.keystore.type'] = 'BCFKS'
@@ -158,7 +159,7 @@ class FilterModule(object):
         return final_dict
 
     # Better logic would be if keystore type is BCFKS
-    def client_properties(self, listener_dict, default_ssl_enabled, default_pkcs12_enabled, default_ssl_mutual_auth_enabled, default_sasl_protocol,
+    def client_properties(self, listener_dict, default_ssl_enabled, keystore_type, default_ssl_mutual_auth_enabled, default_sasl_protocol,
                             config_prefix, truststore_path, truststore_storepass, keystore_path, keystore_storepass, keystore_keypass,
                             omit_jaas_configs, sasl_plain_username, sasl_plain_password, sasl_scram_username, sasl_scram_password,
                             kerberos_kafka_broker_primary, keytab_path, kerberos_principal,
@@ -182,11 +183,17 @@ class FilterModule(object):
             final_dict[config_prefix + 'ssl.keystore.password'] = keystore_storepass
             final_dict[config_prefix + 'ssl.key.password'] = keystore_keypass
 
-        if listener_dict.get('pkcs12_enabled', default_pkcs12_enabled):
+        if keystore_type == 'BCFKS':
             final_dict[config_prefix + 'ssl.keymanager.algorithm'] = 'PKIX'
             final_dict[config_prefix + 'ssl.trustmanager.algorithm'] = 'PKIX'
             final_dict[config_prefix + 'ssl.keystore.type'] = 'BCFKS'
             final_dict[config_prefix + 'ssl.truststore.type'] = 'BCFKS'
+
+        # if listener_dict.get('pkcs12_enabled', default_pkcs12_enabled):
+        #     final_dict[config_prefix + 'ssl.keymanager.algorithm'] = 'PKIX'
+        #     final_dict[config_prefix + 'ssl.trustmanager.algorithm'] = 'PKIX'
+        #     final_dict[config_prefix + 'ssl.keystore.type'] = 'BCFKS'
+        #     final_dict[config_prefix + 'ssl.truststore.type'] = 'BCFKS'
 
         if self.normalize_sasl_protocol(listener_dict.get('sasl_protocol', default_sasl_protocol)) == 'PLAIN' and not omit_jaas_configs:
             final_dict[config_prefix + 'sasl.mechanism'] = 'PLAIN'
