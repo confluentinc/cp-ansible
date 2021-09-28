516234f0f461f5d8fb25a54fa539f8e8a6f11ed4
Justin Manchester <justin.manchester@gmail.com>
2021-09-28 13:45:58 +0100

    Added command to create file that all tests had completed.

--- test_scripts/upgrade.sh
+++ test_scripts/upgrade.sh
@@ -121,3 +121,8 @@ fi
 ## Destroy Infrastructure
 cd roles/confluent.test
 molecule destroy -s $SCENARIO_NAME
+
+## Write success file when complete
+## Note: Upgrade testing in CP-Ansible-Tools repo will check to see if this file exists to know the tests past.
+
+touch all_tests_passed.txt
