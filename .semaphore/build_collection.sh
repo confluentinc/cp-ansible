a9239eea39e239811ed41e1332d58071050c6468
ishikaa-p <ipaliwal@confluent.io>
2026-05-12 15:47:40 +0530

    [ANSIENG-5844] | CI fix

--- .semaphore/build_collection.sh
+++ .semaphore/build_collection.sh
@@ -7,14 +7,15 @@ cd $PATH_TO_CPA
 # prepend the desired default python version to .python-version file
 { echo "$PYTHON_VERSION"; cat .python-version 2>/dev/null || true; } > .python-version.tmp && mv .python-version.tmp .python-version
 
-pip install wheel
-pip install pylint
-pip install "ansible==$ANSIBLE_VERSION"
+# Use the specific Python version for pip installations
+python$PYTHON_VERSION -m pip install wheel
+python$PYTHON_VERSION -m pip install pylint
+python$PYTHON_VERSION -m pip install "ansible==$ANSIBLE_VERSION"
 
-python --version
+python$PYTHON_VERSION --version
 ansible --version
 
-export PYTHON_INTERPRETER=$(which python)
+export PYTHON_INTERPRETER=$(which python$PYTHON_VERSION)
 echo $PYTHON_INTERPRETER
 ansible-galaxy collection build
 
